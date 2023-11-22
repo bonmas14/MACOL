ed0b41bbfa0b73b51dbfb8024e01ddf0d6c588cd
bonmas14 <bonmas14@gmail.com>
2023-11-23 00:31:02 +0300

    Simple interpreter

---
+++ MACOL/Interpreter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MACOL
+{
+    internal class Interpreter
+    {
+        int accumulator = 0;
+        int[] memory = new int[1000];
+
+
+        public void ProcessCode(string code)
+        {
+            for (int i = 0; i < code.Length; i++)
+            {
+
+                switch (code[i])
+                {
+                    case '=':
+                        accumulator = ReadValue(ref i, code);
+                        break;
+                    case '+':
+                        accumulator += ReadValue(ref i, code);
+                        break;
+                    case '-':
+                        accumulator -= ReadValue(ref i, code);
+                        break;
+                    case 'w':
+                        memory[ReadValue(ref i, code)] = accumulator;
+                        break;
+                    case 'r':
+                        accumulator = memory[ReadValue(ref i, code)];
+                        break;
+                    case 'c':
+                        Console.WriteLine(memory[ReadValue(ref i, code)]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public void MemoryDump()
+        {
+            for (int i = 0; i < memory.Length; i++)
+                Console.Write("{0}\t", memory[i]);
+        }
+
+
+        private static int ReadValue(ref int i, string code)
+        {
+            string buffer = "";
+
+            i += 1;
+
+            while (i < code.Length && char.IsDigit(code[i]))
+                buffer += code[i++];
+
+            i -= 1;
+
+            return Convert.ToInt32(buffer);
+        }
+    }
+}
--- MACOL/Program.cs
+++ MACOL/Program.cs
@@ -2,9 +2,41 @@
 {
     internal class Program
     {
+        //        Minimal Absurd Continius Operators Language
+
+        //        все операторы - символы
+        //        все переменные - текст
+        //        типы данных текст - резеврированные слова
+
+        //любая длина переменных
+
+        //типы данных:
+        //s - string
+        //c - char
+        //i - int
+        //f - float
+
+
+        //функция
+        //^имя,вывод:ввод:код^
+
+        //вызов
+        //результат.имя.аргумент.аргумент
+
+        //^main, iout:sarg:out= -1 ^.main
+
+
+        //// вывод в памяти
+        //ia=1,ib=2,ic=a+b
+
+        //сама программа имеет аргументы
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Interpreter interpreter = new Interpreter();
+
+            interpreter.ProcessCode("=200w0=100c0c1");
         }
     }
 }
